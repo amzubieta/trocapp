4af408a12104895d44ba487e2615447828bf3d25
Pedro Lis French <pfrench@vivareal.com>
2018-11-21 11:14:44 -0200

    Adding product info view

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { ProductImagePage } from '../product-image/product-image';
+import { ContactPage } from '../contact/contact';
+import { AboutPage } from '../about/about';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  searchRoot = HomePage;
+  chatRoot = ContactPage;
+  productImageRoot = ProductImagePage;
+  profileRoot = AboutPage;
 
   constructor() {
 
